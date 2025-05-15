cd13152ba97d5bdb1bf15c82b71fadef25532279
noan babao <noan.babao@gmail.com>
2025-05-15 17:26:03 -0500

    test

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,9 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
+app.get("/api/test",  async (req,res)=>{
+    res.status(200).json({message : "Testing"});
+} )
 app.use("/api/auth", authRoutes);
 
 app.listen(PORT,()=> {
